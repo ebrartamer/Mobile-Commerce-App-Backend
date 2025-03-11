a8570be087831206294d3335ef1993323d8c06b9
ebrartamer <ebrartamer21@gmail.com>
2025-03-11 17:00:20 +0300

    Feat: Updated JWT Token Times - Extend Access and Renewal Token Times

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 // Access token oluşturma
 const generateAccessToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '30m' // 30 dakika
+    expiresIn: '2h' // 2 saat
   });
 };
 
 // Refresh token oluşturma
 const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: '7d' // 7 gün
+    expiresIn: '30d' // 30 gün
   });
 };
 
